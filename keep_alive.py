522bde8161ae54becd79ea39440b5efe773f8298
Mak5er <mmaksym.reva@gmail.com>
2023-10-22 12:33:58 +0300

    changed port again

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from threading import Thread
-import requests
 import time
 
 app = Flask('')
@@ -16,7 +15,7 @@ def main():
 
 
 def run():
-    app.run(host="0.0.0.0", port=0000)
+    app.run(host="0.0.0.0", port=10000)
 
 def sleep():
     while True:
@@ -29,28 +28,3 @@ def keep_alive():
     server.start()
     sleepServer = Thread(target=sleep)
     sleepServer.start()
-
-
-def crawl_page(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            pass
-        else:
-            print(f"Помилка: не вдалося отримати сторінку. Код статусу: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"Помилка: {e}")
-
-
-def run_crawler(url, interval):
-    while True:
-        crawl_page(url)
-        time.sleep(interval)
-
-
-def start_crawling():
-    urls = ["https://joke-bot.mak5er.repl.co/", "https://multi-bot.mak5er.repl.co/"]
-    for url_to_crawl in urls:
-        crawl_thread = Thread(target=run_crawler, args=(url_to_crawl, 30))
-        crawl_thread.daemon = True
-        crawl_thread.start()
